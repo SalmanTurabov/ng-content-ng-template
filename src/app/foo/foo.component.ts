9fb536d7a13ee027d1aab9977b8432a25432f80b
Salman <Salmanturab@gmail.com>
2024-01-18 09:48:41 +0300

    ng-content ng-templaate

---
+++ src/app/foo/foo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.scss']
+})
+export class FooComponent {
+
+  @Input()
+  template?: TemplateRef<{$implicit: number}>;
+
+}
